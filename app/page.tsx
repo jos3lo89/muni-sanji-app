384dede3c160059b1d1fcda44a83f1eb83c9af7e
jos3lo89 <ganbaru743@gmail.com>
2025-08-15 22:10:59 -0500

    add: button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>hello world!</h1>;
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  );
 }
